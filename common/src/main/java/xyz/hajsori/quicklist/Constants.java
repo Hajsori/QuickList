76c6baef48074f638dd6380b58a9b9b03f76c7b2
Hajsori <hajsori@proton.me>
2026-05-17 09:47:34 +0200

    Init

---
+++ common/src/main/java/xyz/hajsori/quicklist/Constants.java
@@ -0,0 +1,11 @@
+package xyz.hajsori.quicklist;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "quicklist";
+    public static final String MOD_NAME = "QuickList";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
